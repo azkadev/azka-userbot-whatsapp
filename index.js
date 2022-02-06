3644c993a015fa34a0c94cb5fe45adf83ec584a7
azkadev <insaneazka@gmal.com>
2022-02-06 21:59:48 +0700

    Add demoecho

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var { Whatsapp } = require("whatsapp_client");
 var wa = new Whatsapp();
-wa.on("update", async function(message){
-    var msg = message.messages[0]
-    if (!msg.key.fromMe && message.type === 'notify') {
-        var chat_id = msg.key.remoteJid;
-        return await wa.request("sendMessage", {
-            "chat_id": chat_id,
-            "text": "hello world"
-        });
+wa.on("update", async function (update, update_origin) {
+    if (typeof update == "object") {
+        if (typeof update["message"] == "object") {
+            var msg = update["message"];
+            var chat_id = msg["chat_id"];
+            var text = msg["text"] ?? "";
+            var is_outgoing = msg["is_outgoing"];
+            if (text) {
+                if (RegExp("^/start$", "i").exec(text)) {
+                    return await wa.request("sendMessage", {
+                        "chat_id": chat_id,
+                        "text": "Hay saya adalah robot"
+                    });
+                }
+            }
+        }
     }
 });
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 # Azka Userbot Whatsapp
 
+
+## Demo
+
+https://user-images.githubusercontent.com/82513502/152686817-79a6cf18-796e-4eee-b39c-3d5c8b65101a.mp4
